2199819a25396229c3329d51bc9825594fe30ad4
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2023-10-31 20:51:08 +0530

    Time: 2 ms (99.17%) | Memory: 52.6 MB (46.67%) - LeetSync

---
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isGoodArray(int[] nums) {
+        if(nums.length==0)
+        {
+            return false;
+        }
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            ans=gcd(ans,nums[i]);
+        }
+        if(ans==1) return true;
+        else return false;
+    }
+    public int gcd(int a,int b)
+    {
+        if(a==0)
+        {
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+}
