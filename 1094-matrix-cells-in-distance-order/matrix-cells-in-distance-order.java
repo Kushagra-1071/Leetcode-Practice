1c463312e455a4d4b6a7da778e8da560192c4326
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-01-19 23:52:42 +0530

    Time: 2 ms (100.00%) | Memory: 46.6 MB (12.45%) - LeetSync

--- 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
@@ -1,35 +1,32 @@
 class Solution {
-    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        int[][] ar=new int[rows*cols][2];
-        int cntr=0;
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-            {
-                ar[cntr][0]=i;
-                ar[cntr++][1]=j;
-            }
+    public int[][] allCellsDistOrder(int height, int width, int yCenter, int xCenter) {
+        int maxDistance = Math.max(yCenter, height - yCenter) + Math.max(xCenter, width - xCenter);
+
+        ArrayList<int[]> result = new ArrayList<>();
+        result.add(new int[]{yCenter, xCenter});
+
+        for (int distance = 1; distance <= maxDistance; distance++) {
+            addCellsInDiagonal(height, width, distance, xCenter, yCenter - distance, result, -1, 1);
+            addCellsInDiagonal(height, width, distance, xCenter - distance, yCenter, result, 1, 1);
+            addCellsInDiagonal(height, width, distance, xCenter, yCenter + distance, result, 1, -1);
+            addCellsInDiagonal(height, width, distance, xCenter + distance, yCenter, result, -1, -1);
         }
-        insertionSort(ar,rCenter,cCenter);
-        return ar;
+
+        return result.toArray(new int[][]{});
     }
-    public void insertionSort(int[][]arr,int rC,int cC)
+
+    private static void addCellsInDiagonal(int height, int width, int distance, int x, int y, ArrayList<int[]> result, int xIncrement, int yIncrement) 
     {
-        for(int i=0;i<arr.length-1;i++)
-        {
-            for(int j=i+1;j>0;j--)
-            {
-                if((Math.abs(arr[j][0] - rC) + Math.abs(arr[j][1] - cC)) < (Math.abs(arr[j - 1][0] - rC) + Math.abs(arr[j - 1][1] - cC)))
-                {
-                    swap(arr,j,j-1);
-                }
+        for (int i = distance; i > 0; i--) {
+            if (isWithinBounds(x, y, width, height)) {
+                result.add(new int[]{y, x});
             }
+            x += xIncrement;
+            y += yIncrement;
         }
     }
-    public void swap(int[][] ar,int n1,int n2)
-    {
-        int[] temp=ar[n1];
-        ar[n1]=ar[n2];
-        ar[n2]=temp;
+
+    private static boolean isWithinBounds(int x, int y, int width, int height) {
+        return x >= 0 && y >= 0 && x < width && y < height;
     }
 }
