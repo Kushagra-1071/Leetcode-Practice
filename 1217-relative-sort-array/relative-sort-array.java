500b477451e153f4074f83476b8e0ce28a9c2b8a
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-01-20 21:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (37.51%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] count=new int[1001];
+        for(int i:arr1)
+        {
+            count[i]++;
+        }
+        int n=0;
+        for(int i:arr2)
+        {
+            
+            while(count[i]-->0)
+            {
+                arr1[n++]=i;
+            }
+        }
+        for(int i=0;i<count.length;i++)
+        {
+            while(count[i]-->0)
+            {
+                arr1[n++]=i;
+            }
+        }
+        return arr1;
+    }
+
+}
