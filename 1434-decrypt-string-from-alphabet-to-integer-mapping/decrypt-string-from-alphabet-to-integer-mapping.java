d5112c0d57274678d9628848d649db10132091e5
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-02-01 21:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (71.24%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,31 +1,137 @@
 class Solution {
     public String freqAlphabets(String s) {
-        String s26=s.replace("26#","z");
-        String s25=s26.replace("25#","y");
-        String s24=s25.replace("24#","x");
-        String s23=s24.replace("23#","w");
-        String s22=s23.replace("22#","v");
-        String s21=s22.replace("21#","u");
-        String s20=s21.replace("20#","t");
-        String s19=s20.replace("19#","s");
-        String s18=s19.replace("18#","r");
-        String s17=s18.replace("17#","q");
-        String s16=s17.replace("16#","p");
-        String s15=s16.replace("15#","o");
-        String s14=s15.replace("14#","n");
-        String s13=s14.replace("13#","m");
-        String s12=s13.replace("12#","l");
-        String s11=s12.replace("11#","k");
-        String s10=s11.replace("10#","j");
-        String s9=s10.replace('9','i');
-        String s8=s9.replace('8','h');
-        String s7=s8.replace('7','g');
-        String s6=s7.replace('6','f');
-        String s5=s6.replace('5','e');
-        String s4=s5.replace('4','d');
-        String s3=s4.replace('3','c');
-        String s2=s3.replace('2','b');
-        String s1=s2.replace('1','a');
-        return s1;
+
+       
+        StringBuilder sb = new StringBuilder();
+        // String rs ="";
+        int i = s.length() -1;
+        while(i>=0)
+        {
+            int number =0;
+            if(s.charAt(i)== '#')
+            {
+                number = (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0');
+                i = i-3;
+
+            }
+
+            else{
+                number = (s.charAt(i)-'0');
+                i--;
+            }
+
+            // rs = (char)(number+96) +rs;
+            sb.append((char)(number+96));
+        }
+       
+
+        return sb.reverse().toString();
+
+
+
+
+
+
+
+
+
+
+        //  int number = 0 ;
+        //     if ( s.charAt(i)=='#'){
+        //         number =  (s.charAt(i-2)-'0')*10 + ( s.charAt(i-1)-'0');
+        //         i -= 2;
+        //     }else{
+        //         number = s.charAt(i)-'0';
+        //     }
+        //     ans.append((char) (number+96) );
+
+
+    //     int i = s.length()-1;
+    
+    //     char [] c = s.toCharArray();
+
+    //     String rs ="";
+    //  while(i>=0)
+    //  {
+         
+    //      if(c[i] == '#')
+    //      {
+    //          String ss = "";
+    //          ss = ss + c[i-2] + c[i-1];
+    //          int n = Integer.parseInt(ss);
+    //          rs = alpha(n) + rs; 
+    //          i = i-3;
+    //      }
+
+    //      else{
+
+    //          int t  = c[i] - '0';
+            
+    //          rs =  alpha(t)+ rs;
+    //          i--;
+    //      }
+
+    //  }
+
+    //  return rs;
+
+    // }
+
+    // public String alpha(int n)
+    // {
+    //     if(n == 1)
+    //     return "a";
+    //      else if(n == 2)
+    //     return "b";
+    //      else if(n == 3)
+    //     return "c";
+    //      else if(n == 4)
+    //     return "d";
+    //      else if(n == 5)
+    //     return "e";
+    //      else if(n == 6)
+    //     return "f";
+    //      else if(n == 7)
+    //     return "g";
+    //     else if(n == 8)
+    //     return "h";
+    //     else if(n == 9)
+    //     return "i";
+    //     else if(n == 10)
+    //     return "j";
+    //     else if(n == 11)
+    //     return "k";
+    //     else if(n == 12)
+    //     return "l";
+    //     else if(n == 13)
+    //     return "m";
+    //     else if(n == 14)
+    //     return "n";
+    //     else if(n == 15)
+    //     return "o";
+    //     else if(n == 16)
+    //     return "p";
+    //     else if(n == 17)
+    //     return "q";
+    //     else if(n == 18)
+    //     return "r";
+    //     else if(n == 19)
+    //     return "s";
+    //     else if(n == 20)
+    //     return "t";
+    //     else if(n == 21)
+    //     return "u";
+    //     else if(n == 22)
+    //     return "v";
+    //     else if(n == 23)
+    //     return "w";
+    //     else if(n == 24)
+    //     return "x";
+    //     else if(n == 25)
+    //     return "y";
+
+    //     else
+    //     return "z";
+
     }
 }
