cfccc0b1f69adcc2c89843167ce991531586dd1c
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-02-06 00:09:47 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (79.40%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,11 +1,36 @@
 class Solution {
+
+    public boolean isAlnum(char a) {
+        return (a >= '0' && a <= '9') || (a >= 'a' && a <= 'z');
+    }
+
     public boolean isPalindrome(String s) {
-         s= s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        System.out.println(s);
-        String ss ="";
-        for (int i = s.length()-1; i >=0; i--) {
-            ss+=s.charAt(i);
+        int d = 'a' - 'A';
+
+        for (int i = 0, j = s.length() - 1; i < j;) {
+            char a = s.charAt(i);
+            char b = s.charAt(j);
+
+            if (a >= 'A' && a <= 'Z') {
+                a += d;
+            }
+
+            if (b >= 'A' && b <= 'Z') {
+                b += d;
+            }
+
+            if (!isAlnum(a)) {
+                i++;
+            } else if (!isAlnum(b)) {
+                j--;
+            } else if (a != b) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
         }
-    return(s.equals(ss));
+
+        return true;
     }
 }
