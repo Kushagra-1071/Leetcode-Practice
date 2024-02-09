fae1a06cf666f37f12ac1fda71f461c34e497bf8
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-02-09 22:37:29 +0530

    Time: 6 ms (97.26%) | Memory: 45.3 MB (79.45%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.java
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -1,20 +1,30 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
-        int left,right,farthest=0;
-        Queue<Integer> q=new LinkedList();
-        q.add(0);
-        while(!q.isEmpty())
-        {
-            int k=q.remove();
-            if(k==s.length()-1) return true;
-            left=Math.max(k+minJump,farthest);
-            right=Math.min(k+maxJump,s.length()-1);
-            for(int i=left;i<=right;i++)
-            {
-                if(s.charAt(i)=='0') q.add(i);
+        if(s == null || s.length() == 0) return false;
+        int n = s.length();
+        if(s.charAt(0) == '1' || s.charAt(n - 1) == '1') return false;
+        
+        
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        
+        int start = 0;  //the start index of sliding windows to check
+        int end = 0;  //the end index of sliding windows to check
+        for(int i = 0; i < n; i++){
+            if(!dp[i]){    // quick skip, if current position can't reach
+                continue;
             }
-            farthest=right+1;
+            
+            start = Math.max(end + 1, i + minJump);  // skip the position already checked
+            end = Math.min(n - 1, i + maxJump);  // avoid overflow
+ 
+            for(int j = start; j <= end; j++){
+                if(s.charAt(j) == '0'){
+                    dp[j] = true; 
+                }                
+            }
+            if(dp[n - 1]) return true;
         }
-        return false;
+        return dp[n-1];
     }
 }
