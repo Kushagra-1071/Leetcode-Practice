f1af8d59b073e8bb7b8f08352ebec930b667c33b
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-03-08 23:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (99.73%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,36 +1,14 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        Integer[][][] table=new Integer[nums.length][nums.length][2];
-        int maxScore=recurse(true,table,0,nums.length-1,nums);
+       int n = nums.length;
+        int[] dp = new int[n];
 
-        int total=Arrays.stream(nums).sum();
-
-        return maxScore*2>=total;
-    }
-
-    int recurse(boolean isP1,Integer[][][] table, int start, int end,int[] nums)
-    {
-        if(start>=end) return isP1? nums[start]:0;
-        if(table[start][end][isP1?0:1]!=null)
-        {
-            return table[start][end][isP1?0:1];
+        for (int i=n-1; i>=0; i--) {
+            dp[i] = nums[i];
+            for(int j=i+1; j<n; j++) {
+                dp[j] = Math.max(nums[i]-dp[j],nums[j]-dp[j-1]);
+            }
         }
-
-        if(isP1)
-        {
-            int chooseFirst=recurse(false,table,start+1,end,nums);
-            int chooseLast=recurse(false,table,start,end-1,nums);
-
-            table[start][end][0]=Math.max(chooseFirst+nums[start],chooseLast+nums[end]);
-            return Math.max(chooseFirst+nums[start], chooseLast+nums[end]);
-        }
-
-        else
-        {
-            int chooseFirst=recurse(true,table,start+1,end,nums);
-            int chooseLast=recurse(true,table,start,end-1,nums);
-            table[start][end][1]=Math.min(chooseFirst,chooseLast);
-            return Math.min(chooseFirst,chooseLast);
-        }
+        return dp[n-1]>=0; 
     }
 }
