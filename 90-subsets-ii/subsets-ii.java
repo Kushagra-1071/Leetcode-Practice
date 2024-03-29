4b754df3f90117582bd56b8435c684b0e481ef98
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-03-29 20:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (43.93%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,28 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-                List<List<Integer>> res = new ArrayList<>();
-                Arrays.sort(nums);
-        print(nums,0,new ArrayList<>(),res);
+        List<List<Integer>> res = new ArrayList<>();
+        int[] arr = new int[nums.length];
+
+        Arrays.sort(nums);
+        helper(nums, 0, false, arr, 0, res);
 
         return res;
     }
-    private static void print(int[] arr, int lp, ArrayList<Integer> list, List<List<Integer>> res) {
-        if(!res.contains(list))
-        res.add(new ArrayList<>(list));
 
-        for (int i = lp; i < arr.length; i++) {
-            list.add(arr[i]);
-            print(arr, i + 1, list, res);
-            list.remove(list.size() - 1);
+    public void helper(int[] nums, int cur, boolean taken, int[] arr, int idx, List<List<Integer>> res) {
+        if(cur == nums.length) {
+            List<Integer> add = new ArrayList<>();
+            for (int i = 0; i < idx; i++) {
+                add.add(arr[i]);
+            }
+            res.add(add);
+            return;
         }
+
+        if(!(cur > 0 && !taken && nums[cur - 1] == nums[cur])) {
+            arr[idx] = nums[cur];
+            helper(nums, cur + 1, true, arr, idx + 1, res);
+        }
+        helper(nums, cur + 1, false, arr, idx, res);
     }
 }
