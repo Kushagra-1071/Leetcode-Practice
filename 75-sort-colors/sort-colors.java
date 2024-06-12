faaa992131dab99ff26a0f7be9c3818eca46cd58
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-06-12 21:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (97.20%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,18 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int temp=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums.length-1-i;j++)
-            {
-                if(nums[j]>nums[j+1])
-                {
-                    temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
-            }
-        }
+        Arrays.sort(nums);
+        
     }
 }
