155022adfa868061802964033e01ee0822eaa419
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-07-09 15:16:29 +0530

    Time: 3 ms (93.06%) | Memory: 72.7 MB (58.37%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,18 +1,12 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int n=customers.length;
-        double time_waiting=customers[0][1];
-        int finished_prev=customers[0][0]+customers[0][1];
-        for(int customer_ind=1;customer_ind<n;++customer_ind)
-        {
-            int[] times=customers[customer_ind];
-            int arrive=times[0];
-
-            int start_cook=Math.max(arrive,finished_prev);
-            int end_time=start_cook+times[1];
-            finished_prev=end_time;
-            time_waiting+=end_time-arrive;
+        double wait = 0;
+        int t = -1;
+        for(int[] a : customers) {
+            t = (t<a[0])?a[0]:t;
+            t += a[1];
+            wait += t-a[0];
         }
-        return time_waiting/n;
+        return wait/customers.length;
     }
 }
