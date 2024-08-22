8d3a6b174b0f6484bbce291afe926a80f9d5e844
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2024-08-22 23:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (90.99%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,16 +1,11 @@
 class Solution {
     public int findComplement(int num) {
-        int tmp=(int)(Math.pow (2,digits(num))-1);
-        return tmp-num;
-    }
-    static int digits(int n)
-    {
-        int count=0;
-        while(n>0)
-        {
-            n=n/2;
-            count++;
-        }
-        return count;
+        if(num==0)
+        return 1;
+
+        int bitlength=Integer.toBinaryString(num).length();
+        int mask=(1 << bitlength)-1;
+
+        return num^mask;
     }
 }
