bc9796b853bec29a97874c3cf26eb8e3583b8882
Kushagra Dhingra <79652829+Kushagra-1071@users.noreply.github.com>
2026-02-06 23:57:40 +0530

    Time: 8 ms (90.15%) | Memory: 49 MB (68.24%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		
-		for (int i = 0; i < intervals.length; i++) {
-			min = Math.min(min, intervals[i][0]);
-			max = Math.max(max, intervals[i][0]);
-		}
-		
-		int[] range = new int[max - min + 1];
-		for (int i = 0; i < intervals.length; i++) {
-			range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]); 
-		}
-		
-		int start = 0, end = 0;
-		LinkedList<int[]> result = new LinkedList<>();
-		for (int i = 0; i < range.length; i++) {
-			if (range[i] == 0) {
-				continue;
-			}
-			if (i <= end) {
-				end = Math.max(range[i], end);
-			} else {
-				result.add(new int[] {start + min, end + min});
-				start = i;
-				end = range[i];
-			}
-		}
-		result.add(new int[] {start + min, end + min});
-		return result.toArray(new int[result.size()][]);
+		Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        LinkedList<int[]> merged=new LinkedList<>();
+        for(int[] interval:intervals)
+        {
+            if(merged.isEmpty()||merged.getLast()[1]<interval[0])
+            {
+                merged.add(interval);
+            }
+            else
+            {
+                merged.getLast()[1]=Math.max(merged.getLast()[1],interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
 	}
 }
